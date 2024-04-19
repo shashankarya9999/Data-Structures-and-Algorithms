9cf2cb8ef549c62cb98dda5d1307c4880c4fe2a4
Shashank Arya <shashankarya9831@gmail.com>
2024-04-19 22:36:36 +0530

    Update max_pairwise_product.cpp

--- 01-Algorithmic-Toolbox/01-Algorithmic-Warm-Up/max_pairwise_product.cpp
+++ 01-Algorithmic-Toolbox/01-Algorithmic-Warm-Up/max_pairwise_product.cpp
@@ -10,11 +10,11 @@ int max_pairwise_product(const vector<int>& numbers)
     int result = 0;
     int n = numbers.size();
 
-    for(int i=0; i < n; ++i)
+    for( int i=0; i<n; ++i )
     {
-        for(int j=i+1; j<n; ++j)
+        for( int j=i+1; j<n; ++j )
         {
-            if(numbers[i] * numbers[j] > result)
+            if( numbers[i] * numbers[j] > result )
             {
                 result = numbers[i] * numbers[j];
             }
@@ -34,7 +34,7 @@ int main()
     vector<int> numbers(n);
 
     cout << "Enter the elements: ";
-    for(int i=0; i<n; ++i)
+    for( int i=0; i<n; ++i )
     {
         cin >> numbers[i];
     }
