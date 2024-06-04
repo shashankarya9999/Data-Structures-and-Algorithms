586db675e2f661988ddc6d766f055edac9bb5091
Shashank Arya <shashankarya9831@gmail.com>
2024-06-04 07:19:17 +0530

    Update max_pairwise_product.cpp

--- 01-Algorithmic-Toolbox/01-Programming-Challenges/max_pairwise_product.cpp
+++ 01-Algorithmic-Toolbox/01-Programming-Challenges/max_pairwise_product.cpp
@@ -5,23 +5,53 @@ using std::vector;
 using std::cin;
 using std::cout;
 
-int max_pairwise_product(const vector<int>& numbers)
+// this solution is quite slow
+// our algorithm goes through all possible pairs of input numbers
+// need for better algorithm
+
+long long int max_pairwise_product(const vector<int> &numbers)
 {
-    int result = 0;
+    long long int result = 0;
     int n = numbers.size();
 
     for( int i=0; i<n; ++i )
     {
         for( int j=i+1; j<n; ++j )
         {
-            if( numbers[i] * numbers[j] > result )
+            if(((long long)numbers[i]) * numbers[j] > result)
             {
-                result = numbers[i] * numbers[j];
+                result = ((long long)numbers[i]) * numbers[j];
             }
         }
     }
 
     return result;
+}
+
+// improving the naive solution
+// idea: that to find the maximum product of two numbers from our sequence,
+// it is actually enough to find the two maximal numbers in the sequence
+// this is because all numbers in our sequence are non-negative
+
+long long max_pairwise_product_fast(const vector<int> &numbers)
+{
+    int n = numbers.size();
+
+    int max_index1 = -1;
+    for(int i=0; i<n; ++i)
+    {
+        if((max_index1 == -1) || (numbers[i] > numbers[max_index1]))
+            max_index1 = i;
+    }
+
+    int max_index2 = -1;
+    for(int j=0; j<n; ++j)
+    {
+        if((numbers[j] != numbers[max_index1]) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
+            max_index2 = j;
+    }
+
+    return ((long long)(numbers[max_index1])) * numbers[max_index2];
 }
 
 int main()
@@ -36,8 +66,10 @@ int main()
     cout << "Enter the elements: ";
     for( int i=0; i<n; ++i )    cin >> numbers[i];
 
-    int result = max_pairwise_product(numbers);
-    cout << "The maximum pairwise product = " << result << endl;
+    long long int result = max_pairwise_product(numbers);
+    long long int result2 = max_pairwise_product_fast(numbers);
+    cout << "The maximum pairwise product = " << result << "\n";
+    cout << "The maximum pairwise product = " << result2 << "\n";
 
     return 0;
 }
